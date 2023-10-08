8bce22bb6d879f3ea8933b6d4e68112e5fd15440
Tiosphere <loong_22@hotmail.com>
2023-10-08 15:55:14 +0700

    add models

---
+++ backend/src/main/java/com/scalable/toktik/model/extend/AbstractModel.java
@@ -0,0 +1,25 @@
+package com.scalable.toktik.model.extend;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+
+@MappedSuperclass
+@Getter
+@Setter
+public class AbstractModel {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    protected Long id;
+    @Column(name = "created", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime created = LocalDateTime.now();
+}
