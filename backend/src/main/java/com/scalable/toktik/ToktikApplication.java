10833c30daa2b877a334c483cc7af0ded401fab9
Tiosphere <loong_22@hotmail.com>
2023-10-08 13:31:05 +0700

    add backend

---
+++ backend/src/main/java/com/scalable/toktik/ToktikApplication.java
@@ -0,0 +1,13 @@
+package com.scalable.toktik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToktikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToktikApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/scalable/toktik/ToktikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalable.toktik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToktikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
