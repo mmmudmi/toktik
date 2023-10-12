75e2fa662ee442581186388b4a957c1824a77a0f
Mudmi <mmmudmi@gmail.com>
2023-10-12 22:00:22 +0700

    welcome page done

---
+++ frontend/src/views/Welcome.vue
@@ -0,0 +1,54 @@
+<template>
+  <div style="display: flex; justify-content: center;align-items: center;height: 100vh;">
+    <div class="container">
+        <v-img src="src/images/toktik.png" style="scale: 0.9;"></v-img>
+      <v-row class="lower">
+
+        <v-btn class="black-btn" style="margin: 0 1pc 0 1pc;width: 7pc;" @click="navigateToLogin">login</v-btn>
+        <v-btn class="black-btn" style="margin: 0 1pc 0 1pc;width: 7pc;" @click="navigateToRegister">register</v-btn>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script >
+import axios from 'axios'
+export default {
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+    navigateToRegister(){ this.$router.push('signUp'); },
+    navigateToLogin(){ this.$router.push('login'); },
+  },
+  mounted() {
+
+  },
+  beforeMount() {
+  }
+}
+</script>
+
+<style scoped>
+@import '@/styles/btn-style.css';
+.container{
+  width: 27pc;
+  height: 27pc;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  top: -3pc;
+}
+.upper{
+  flex: 2;
+  object-fit: contain;
+}
+.lower{
+  flex: 1;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+</style>
