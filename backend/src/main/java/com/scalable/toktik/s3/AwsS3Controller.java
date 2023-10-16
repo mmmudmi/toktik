8dd8278d39907a6a2d606c997adb44bccb8a6473
Tiosphere <loong_22@hotmail.com>
2023-10-16 13:39:02 +0700

    minor redis connection fix

--- backend/src/main/java/com/scalable/toktik/s3/AwsS3Controller.java
+++ backend/src/main/java/com/scalable/toktik/s3/AwsS3Controller.java
@@ -4,6 +4,7 @@ import com.amazonaws.HttpMethod;
 import com.scalable.toktik.record.response.BoolResponse;
 import com.scalable.toktik.record.s3.S3CompleteForm;
 import com.scalable.toktik.record.s3.S3RequestForm;
+import com.scalable.toktik.redis.RedisService;
 import com.scalable.toktik.service.UserService;
 import com.scalable.toktik.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +21,17 @@ import java.util.UUID;
 public class AwsS3Controller {
 
     private final AwsS3Service awsS3Service;
+    private final RedisService redisService;
     private final VideoService videoService;
     private final UserService userService;
     @Value("${aws.bucketName}")
     private String bucketName;
+    private final String queueName = "video_queue";
 
     @Autowired
-    public AwsS3Controller(AwsS3Service awsS3Service, VideoService videoService, UserService userService) {
+    public AwsS3Controller(AwsS3Service awsS3Service, RedisService redisService, VideoService videoService, UserService userService) {
         this.awsS3Service = awsS3Service;
+        this.redisService = redisService;
         this.videoService = videoService;
         this.userService = userService;
     }
@@ -46,10 +50,11 @@ public class AwsS3Controller {
     @PostMapping("/upload-complete")
     public BoolResponse uploadComplete(S3CompleteForm s3CompleteForm, @AuthenticationPrincipal UserDetails userDetails) {
         videoService.createVideo(s3CompleteForm.filename(), s3CompleteForm.caption(), userService.findByUsername(userDetails.getUsername()));
+        redisService.sendMessageToQueue(queueName, s3CompleteForm.filename());
         return new BoolResponse(true, "Successfully recorded");
     }
 
-    @GetMapping("/count")
+    @GetMapping("/list")
     public BoolResponse count() {
         return new BoolResponse(true, awsS3Service.getObjectList(bucketName).toString());
     }
