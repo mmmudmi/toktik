c4c8dc20c2d23eab7a5e5ffc258354045f56e5d2
Mudmi <mmmudmi@gmail.com>
2023-11-01 23:19:31 +0700

    nav

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,14 +4,20 @@
       <v-toolbar-title>
         <v-row align="start">
           <v-col class="shrink">
-            <v-img src="https://scalable.ap-south-1.linodeobjects.com/toktik_wide.png" style="cursor: pointer;margin-left:1pc" width="100" @click="navigateToHomePage"></v-img>
+            <a href="/home">
+              <v-img src="https://scalable.ap-south-1.linodeobjects.com/toktik_wide.png" style="cursor: pointer;margin-left:1pc" width="100" ></v-img>
+            </a>
           </v-col>
           <v-col align="end">
-            <v-btn class="reg-btn" style="width: 6.2pc" @click="navigateToMyVideosPage"> My videos </v-btn>
+            <a href="/myVideos" style="color: black;">
+              <v-btn class="reg-btn" style="width: 6.2pc"> My videos </v-btn>
+            </a>
+            <a href="/upload" style="color: black;">
             <v-btn class="reg-btn" style="width: 6.2pc" @click="navigateToUploadPage">
               <i class="fa fa-plus" style="font-size:12px; padding-right: 5px"></i>
               Upload
             </v-btn>
+            </a>
             <v-btn class="red-btn" style="width: 6.2pc" @click="logout"> Log out</v-btn>
           </v-col>>
         </v-row>
@@ -57,5 +63,6 @@ export default {
 .Navbar{
   background-color: #ffffff;
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.2);
+  z-index: 10;
 }
 </style>
